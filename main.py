4aab1dbe25e723cfbd0683468ce7da74c9a5562e
Maxim <108695003+SaixFix@users.noreply.github.com>
2023-09-08 11:16:59 +0300

    added while loop with choice in main function

--- main.py
+++ main.py
@@ -16,37 +16,44 @@ def main():
         'телефон личный (сотовый)': '5'
     }
 
-    user_choice: str = input(
-        'введите:\n 1 если хотите добавить,\n 2 для просмотра всего справочника,\n'
-        ' 3 Поиск записей по одной или нескольким характеристикам и редактирование\n '
-    )
+    exit_func = False  # переменная для выхода из цикла
 
-    if user_choice == '1':
+    while not exit_func:
+        user_choice: str = input(
+            'введите:\n 1 если хотите добавить,\n 2 для просмотра всего справочника,\n'
+            ' 3 Поиск записей по одной или нескольким характеристикам и редактирование\n'
+            ' 4 для выхода из программы\n'
+        )
+        if user_choice == '4':
+            exit_func = True
+        if user_choice == '1':
 
-        # Записываем в словарь новые данные от юзера
-        for row in data:
-            data[row]: str = input(f'{row} ')
+            # Записываем в словарь новые данные от юзера
+            for row in data:
+                data[row]: str = input(f'{row} ')
 
-        all_data.append(data)
-        rewrite_data(all_data)
+            all_data.append(data)
+            rewrite_data(all_data)
+            continue
 
-    if user_choice == '2':
-        # можно установить размер страницы больше 1
-        page_size: int = 2
-        total_page: int = round(len(all_data) / page_size)
-        print(f'всего страниц  {total_page}')
+        if user_choice == '2':
+            # можно установить размер страницы больше 1
+            page_size: int = 2
+            total_page: int = round(len(all_data) / page_size)
+            print(f'всего страниц  {total_page}')
 
-        paginating(all_data, page_size, total_page)
+            paginating(all_data, page_size, total_page)
 
-    if user_choice == '3':
-        result_search = search(all_data)
-        step_2 = input('Желаете отредактировать найденный результат, введите да или нет\n').lower()
-        if step_2 == 'да':
-            if len(result_search) > 1:
-                print('Найдено больше 1 результата попробуйте заново, уточните данные дня поиска')
-            if len(result_search) == 1:
-                redact_one(data, result_search)
-                rewrite_data(all_data)
+        if user_choice == '3':
+            result_search = search(all_data)
+            step_2 = input('Желаете отредактировать найденный результат, введите да или нет\n').lower()
+            if step_2 == 'да':
+                if len(result_search) > 1:
+                    print('Найдено больше 1 результата попробуйте заново, уточните данные дня поиска')
+                if len(result_search) == 1:
+                    redact_one(data, result_search)
+                    rewrite_data(all_data)
+                    continue
 
 
 if __name__ == "__main__":
